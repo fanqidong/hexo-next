545b676fa403a3a6cbb07964943a8d1c0895fc09
fanqidong <1425434413@qq.com>
2018-09-05 18:24:38 +0800

    update

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,11 +116,11 @@
                 var data = res.list[j].arr;
                 var liTmpl = "";
                 for (var i = 0, len = data.link.length; i < len; i++) {
-                    var minSrc = 'https://github.com/fanqidong/hexo-next/blob/master/min_photos/' + data.link[i];
-                    var src = 'https://github.com/fanqidong/hexo-next/blob/master/photos/' + data.link[i];
+                    var minSrc = 'https://raw.githubusercontent.com/fanqidong/hexo-next/master/min_photos/' + data.link[i];
+                    var src = 'https://raw.githubusercontent.com/fanqidong/hexo-next/master/photos/' + data.link[i];
                     var type = data.type[i];
                     var target = src + (type === 'video' ? '.mp4' : '.jpg');
-                    src += '.jpg';
+                    // src += '.jpg';
 
                     liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                   <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
